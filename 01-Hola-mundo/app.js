27abe0d931eb1a457fdfb04384b2c1df0e595e59
victor_global <vmmrodadotest@gmail.com>
2025-04-23 12:57:14 +0200

    added hola mundo

---
+++ 01-Hola-mundo/app.js
@@ -0,0 +1,5 @@
+//Declarar variables
+var saludo = "Hola, mundo desde let!";
+var SALUDO = "Hola, mundo desde constante!";
+console.log(saludo);
+console.log(SALUDO);
---
+++ 01-Hola-mundo/app.ts
@@ -0,0 +1,18 @@
+//Declarar variables
+let saludo: string = "Hola, mundo desde let!";
+const SALUDO: string = "Hola, mundo desde constante!";
+
+console.log(saludo);
+console.log(SALUDO);
+
+
+
+
+
+
+
+
+
+
+
+
