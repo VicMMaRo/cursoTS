b2f7b4fac9382ede22d8751e40f3540af2919b9c
victor_global <vmmrodadotest@gmail.com>
2025-04-23 14:10:49 +0200

    update

---
+++ 02-tsconfig-y-watch-mode/app.js
@@ -0,0 +1,5 @@
+"use strict";
+//Para crear el archivo tsconfig.json inicializamos typescrit en la carpeta raíz de nuestro proyecto.
+//para ello en la consola ejecutamos el comando "tsc --init"
+//Wacth
+// Para compilar y observar los cambios en tiempo real, ejecutamos el comando "tsc --watch".
