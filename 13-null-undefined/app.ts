f7a8698f53d0a9140fbbcdb0a62d80eeba54c972
victor_global <vmmrodadotest@gmail.com>
2025-04-25 11:54:35 +0200

    added 13-nunn-undefined

---
+++ 13-null-undefined/app.ts
@@ -0,0 +1,42 @@
+//Null y Undefined
+let tipo: any = (():(number | undefined) => {
+    let res;
+  
+    res = null;
+    res = 9/0;
+    res = "el perro del hortelano" / 4;
+    
+    // console.log({res}, typeof res);
+  
+    if (typeof res === "number") {
+      switch (res) {
+        case Infinity:
+        case -Infinity:
+        case NaN:
+          return {res: res, tipo: typeof res}
+        default:
+          return;
+      }
+    }
+  });
+  
+  let resTipo;
+  resTipo = tipo()
+  
+  console.log({resTipo}, typeof resTipo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
