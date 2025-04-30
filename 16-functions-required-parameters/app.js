ac292511d993abd8c57b1d1c41c40696f696e533
victor_global <vmmrodadotest@gmail.com>
2025-04-30 10:09:33 +0200

    added 16-funtions

---
+++ 16-functions-required-parameters/app.js
@@ -0,0 +1,57 @@
+// import { sepCorto, sepLargo } from "./separadores.js";
+import * as sep from './separadores.js';
+sep.sepCorto();
+function isValidStringParam(param) {
+    // if (param.length > 0) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    /**
+     * Alternativa IMPLICITA al if .. else
+     *
+     * return param.length > 0;
+     */
+    return param.length > 0 && param !== 'null'; // true | false
+}
+const fullName = (firstName, lastName) => {
+    let resEvalName = {
+        boolFirstName: isValidStringParam(firstName),
+        boolLastName: isValidStringParam(lastName),
+    };
+    console.log("resEvalName:", resEvalName);
+    if (resEvalName.boolFirstName && resEvalName.boolLastName) {
+        return `${firstName} ${lastName}`;
+    }
+    else {
+        if (!resEvalName.boolFirstName && !resEvalName.boolLastName) {
+            return 'No has proporcionado ni el nombre ni el apellido';
+        }
+        else if (!resEvalName.boolFirstName) {
+            return 'No has proporcionado el nombre';
+        }
+        else if (!resEvalName.boolLastName) {
+            return 'No has proporcionado el apellido';
+        }
+    }
+    return 'No se han producido errores';
+};
+let firstName;
+let lastName;
+let cont;
+let res;
+do {
+    firstName = String(prompt('Nombre:'));
+    lastName = String(prompt('Apellido:'));
+    res = fullName(firstName, lastName);
+    console.log(res);
+    cont = prompt('Desea continuar [s/n]');
+} while (cont === 's' || cont === 'S');
+/**
+ * Alternativa para evaluar los parámetros vacíos arrojando
+ * un error y deteniendo la aplicación
+ *
+ * if (!firstName || !lastName) {
+ *   throw new Error('No has proporcionado nombre y/o apellidos')
+ * }
+ */
