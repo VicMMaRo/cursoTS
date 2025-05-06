d76064761792cc1d1faab5738aa7fddbc3fbba6e
victor_global <vmmrodadotest@gmail.com>
2025-05-06 12:11:02 +0200

    added secction 24

---
+++ 24-ejercicio-tipado-types/app.ts
@@ -0,0 +1,104 @@
+import * as separadores from './separadores.js'
+//
+(() => {
+
+  //creamos un type tipado para los vehículos
+  type Vehiculo = {
+    carroceria: string,
+    modelo: string,
+    antibalas: boolean,
+    pasajeros: number,
+    disparar?: () => string | null
+  }
+
+  // Objetos: le asignamos el type vehículo
+const batimovil: Vehiculo = {
+  carroceria: "Negra",
+  modelo: "6x6",
+  antibalas: true,
+  pasajeros:4
+};
+
+const bumblebee: Vehiculo = {
+  carroceria: "Amarillo con negro",
+  modelo: "4x2",
+  antibalas: true,
+  pasajeros:4,
+  disparar(){ // El metodo disparar es opcional
+    console.log("Disparando");
+  }
+};
+
+
+// Villanos debe de ser un arreglo de objetos personalizados
+
+//creamos el tipo Villanos y se lo asignamos al array de objetos villanos
+type Villanos = {
+  nombre:string,
+  edad: number | undefined,//mejor no usar undefined y usar null en su lugar. Que undefined siempre sea un valor devuelto por js y no por el programador
+  mutante: boolean
+}
+
+const villanos: Villanos[] = [{
+  nombre:"Lex Luthor",
+  edad: 54,
+  mutante:false
+},{
+  nombre: "Erik Magnus Lehnsherr",
+  edad: 49,
+  mutante: true
+},{
+  nombre: "James Logan",
+  edad: undefined,
+  mutante: true
+}];
+
+// Multiples tipos
+// cree dos tipos, uno para charles y otro para apocalipsis
+
+type Charles = {
+  poder: string,
+  estatura: number
+}
+
+const charles: Charles = {
+  poder:"psiquico",
+  estatura: 1.78
+};
+
+type Apocalipsis = {
+  lider: boolean,
+  miembros: string[]
+}
+
+const apocalipsis: Apocalipsis = {
+  lider:true,
+  miembros: ["Magneto","Tormenta","Psylocke","Angel"]
+}
+
+// Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
+let mystique;
+
+mystique = charles;
+console.log(mystique);
+mystique = apocalipsis;
+console.log(mystique);
+
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
