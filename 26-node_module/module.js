3e44a702f4101c1b6b868efc638b347513ca3d41
victor_global <vmmrodadotest@gmail.com>
2025-05-09 12:22:14 +0200

    added 26-node

---
+++ 26-node_module/module.js
@@ -0,0 +1,6 @@
+//
+(() => {
+    //mirar repo profesor para hacer esta sección. Esta dentro de la carpeta 25, no como una nueva carpeta 26
+})();
+export {};
+//# sourceMappingURL=module.js.map
---
+++ 26-node_module/module.ts
@@ -0,0 +1,21 @@
+import * as separadores from './separadores.js'
+//
+(() => {
+//mirar repo profesor para hacer esta sección. Esta dentro de la carpeta 25, no como una nueva carpeta 26
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
